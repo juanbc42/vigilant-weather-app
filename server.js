34a993d688503cee48d2846a93914a3d84f0846e
Juan Carvalho <juanbc@ciandt.com>
2022-03-22 12:31:18 -0300

    changed app ports to 80

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //requires js files
 var ExpressServer = require('./ExpressServer.js');
 
-ExpressServer().listen(8080, function () {
-  console.log('Weather app listening on port 8080!');
+ExpressServer().listen(80, function () {
+  console.log('Weather app listening on port 80!');
 })
