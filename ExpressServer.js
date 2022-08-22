820816e288c03773979a044277e892bfe5e433f9
Juan Barreto de Carvalho <juanbarret@gmail.com>
2022-08-22 09:45:36 -0300

    [secret mgmt]adding apikey to a different file

--- ExpressServer.js
+++ ExpressServer.js
@@ -1,12 +1,14 @@
+const apiKey = require('./apikey.js');
 const BodyCreate = require('./public/js/BodyCreate.js');
+
 var ExpressServer = function ExpressServer(){
-    const apiKey = '9a3387712c45666838557f30ce54b225';
+    
     const axios = require('axios');
     const { render } = require('ejs');
     const express = require('express');
     const bodyParser = require('body-parser');
     const path = require('path');
-    const app = express(); //inicializa o express server como 'app'
+    const app = express(); // inicializa o express server como 'app'
     
     app.use('/static', express.static('public'));
     app.set('view engine', 'ejs');
@@ -21,12 +23,12 @@ var ExpressServer = function ExpressServer(){
     });
 
     app.post('/', async function (req, res) {
-        let w_city = req.body.city; //necessário para buscar do html o conteudo de city
+        let w_city = req.body.city; // necessário para buscar do html o conteudo de city
         let w_units = req.body.units;
         console.log ("antes do try")
         try {
             console.log ("entrou no try")
-                const weatherAPI = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${w_city}&units=${w_units}&appid=${apiKey}`);
+                const weatherAPI = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${w_city}&units=${w_units}&appid=${apiKey()}`);
                 res.render('index', BodyCreate(weatherAPI, w_units));
                 const obj = JSON.stringify(weatherAPI.data);
                 console.log (obj);
